2a7bca3b097e085ced10a1cc3415279fc20153eb
KazuakiHyoda <kazuaki@ex-t.jp>
2018-08-05 20:27:13 +0900

    gitignore用のスクリプトを追加 gitignoreでトラッキングから除去できない場合に利用

---
+++ gitignore.sh
@@ -0,0 +1,22 @@
+git rm --cached *.csproj
+git rm --cached *.unityproj
+git rm --cached *.sln
+git rm --cached *.suo
+git rm --cached *.tmp
+git rm --cached *.user
+git rm --cached *.userprefs
+git rm --cached *.pidb
+git rm --cached *.booproj
+git rm --cached *.svd
+git rm --cached *.pidb.meta
+git rm --cached sysinfo.txt
+git rm --cached *.apk
+git rm --cached *.unitypackage
+git rm --cached Assets/AssetStoreTools*
+
+git rm --cached -r [Ll]ibrary/
+git rm --cached -r [Tt]emp/
+git rm --cached -r [Oo]bj/
+git rm --cached -r [Bb]uild/
+git rm --cached -r [Bb]uilds/
+git rm --cached -r ExportedObj/
